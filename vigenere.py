47ce410c45abaad57e55b2950ff911ef62b0b4c6
Kaylee <kayleeyin2@gmail.com>
2020-10-18 00:52:48 -0400

    basic idea down

--- vigenere.py
+++ vigenere.py
@@ -2,4 +2,15 @@
 Kaylee Yin
 Cybersecurity - Period 5
 October 18, 2020
+
 '''
+import sys
+
+#True = encode, False = decode
+def vigenere(text, key, code):
+    keyLength = len(key)
+    index = 0 #index relating to the key
+
+    for i in range(len(text)):
+        if code == True:
+            if
