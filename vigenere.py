2df27ddf8d6d9a32a606920bdb390cf07f940d1b
Kaylee <kayleeyin2@gmail.com>
2020-10-18 01:40:20 -0400

    cleaner version of encode/decode

--- vigenere.py
+++ vigenere.py
@@ -8,6 +8,7 @@ import sys
 
 #True = encode, False = decode
 def vigenere(text, key, code):
+    text = text.upper() #make all characters uppercase
     for i in text: #ensure only capitalized letters are included
         if ord(i) < 65 or ord(i) > 90:
             text = text.replace(i, "")
@@ -17,6 +18,25 @@ def vigenere(text, key, code):
     final = ""
 
     for i in range(len(text)):
+        if index == keyLength:
+            index = 0
+        textIndex = int(ord(text[i]))
+        keyIndex = int(ord(key[index])) - 65
+
+        if code == True:
+            newIndex = textIndex + keyIndex
+            if newIndex > 90: #circle back to the beginning of the alphabet
+                newIndex -= 26
+
+        else:
+            newIndex = textIndex - keyIndex
+            if newIndex < 65: #circle to the end of the alphabet
+                newIndex = 26 + (textIndex - keyIndex)
+
+        index += 1
+        final += chr(newIndex)
+
+        '''
         if code == True: #encode, add letters
             if index == keyLength:
                 index = 0
@@ -42,5 +62,7 @@ def vigenere(text, key, code):
 
             index += 1
             final += chr(newIndex)
-
+        '''
     print(final)
+
+vigenere('hotdogstand', 'BOAR', True) #test code
