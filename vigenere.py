ae3922be76a4da6ce4d9b1558da785dfe9a090d4
Kaylee <kayleeyin2@gmail.com>
2020-10-18 01:12:29 -0400

    encode complete

--- vigenere.py
+++ vigenere.py
@@ -10,7 +10,31 @@ import sys
 def vigenere(text, key, code):
     keyLength = len(key)
     index = 0 #index relating to the key
+    final = ""
 
     for i in range(len(text)):
-        if code == True:
-            if
+        if code == True: #encode, add letters
+            if index == keyLength:
+                index = 0
+            textIndex = int(ord(text[i]))
+            keyIndex = int(ord(key[index])) - 97
+            newIndex = textIndex + keyIndex
+
+            if newIndex > 122: #circle back to the beginning of the alphabet
+                newIndex -= 26
+
+            index += 1
+            final += chr(newIndex)
+
+        if code == False: #decode, subtract letters
+            if index == keyLength:
+                index = 0
+            textIndex = int(ord(text[i]))
+            keyIndex = int(ord(key[index])) - 97
+            newIndex = textIndex + keyIndex
+
+            if newIndex < 97: #circle to the end of the alphabet
+                newIndex -= 26
+
+            index += 1
+            final += chr(newIndex)
